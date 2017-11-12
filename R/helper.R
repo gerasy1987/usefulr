713970ee54cee614c0a83be2f923d578e2fea984
gerasy1987 <gs2716@columbia.edu>
2017-11-12 17:08:35 +0700

    condence helper functions into single к-ыскшзе

--- R/helper.R
+++ R/helper.R
@@ -37,3 +37,24 @@ add_stars <- function(p.value) {
                          ifelse(p.value <= 0.15, "+", ""))))
   )
 }
+
+#' FUNCTION_TITLE
+#'
+#' FUNCTION_DESCRIPTION
+#'
+#' @param .matrix DESCRIPTION.
+#' @param .names DESCRIPTION.
+#'
+#' @return RETURN_DESCRIPTION
+#' @examples
+#' # ADD_EXAMPLES_HERE
+make_df <- function(.matrix,
+                    .names = c("estimate", "std.error", "p.value", "term")) {
+
+  requireNamespace("dplyr", quietly = TRUE)
+
+  df <- dplyr::as_data_frame(.matrix)
+  df$term <- rownames(.matrix)
+  names(df) <- .names
+  return(df)
+}
