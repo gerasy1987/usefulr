c71adf2bc075074e4a44849f5dfa4d8d8aecbfe9
gerasy1987 <gs2716@columbia.edu>
2018-04-19 11:36:32 -0400

    add wtd_sd and correct examples

--- R/wtd_mean.R
+++ R/wtd_mean.R
@@ -2,18 +2,17 @@
 #'
 #' @param x A numeric vector.
 #' @param weights A numeric vector of weights.
-#' @param normwt Specify normwt=TRUE to make weights sum to length(x) after deletion of NAs.
 #' @param na.rm Set to FALSE to suppress checking for NAs
 #' @return Returns scalar vector of length one.
 #' @examples
 #' \dontrun{set.seed(1)
 #'          x <- runif(500)
 #'          wts <- sample(1:6, 500, TRUE)
-#'          mn <- wtd.mean(x = x, weights = wts)}
+#'          mn <- wtd_mean(x = x, weights = wts)}
 #'
 #' @export
 
-wtd_mean <- function (x, weights = NULL, normwt = "ignored", na.rm = TRUE) {
+wtd_mean <- function (x, weights = NULL, na.rm = TRUE) {
   if (!length(weights))
     return(mean(x, na.rm = na.rm))
   if (na.rm) {
@@ -22,4 +21,39 @@ wtd_mean <- function (x, weights = NULL, normwt = "ignored", na.rm = TRUE) {
     weights <- weights[s]
   }
   return(sum(weights * x)/sum(weights))
+}
+
+
+#' Compute weighted standard deviation.
+#'
+#' @param x A numeric vector.
+#' @param weights A numeric vector of weights.
+#' @param na.rm Set to FALSE to suppress checking for NAs
+#' @return Returns scalar vector of length one.
+#' @examples
+#' \dontrun{set.seed(1)
+#'          x <- runif(500)
+#'          wts <- sample(1:6, 500, TRUE)
+#'          mn <- wtd_mean(x = x, weights = wts)
+#'          sd <- wtd_sd(x = x, weights = wts)}
+#'
+#' @export
+
+wtd_sd <- function (x, weights = NULL, na.rm = TRUE) {
+  if (!length(weights)) {
+    if (na.rm)
+      x <- x[!is.na(x)]
+    return(sd(x, na.rm = na.rm))
+  }
+  if (na.rm) {
+    s <- !is.na(x + weights)
+    x <- x[s]
+    weights <- weights[s]
+  }
+  sw <- sum(weights)
+
+  if (sw <= 1) warning("only one effective observation; variance estimate undefined")
+
+  xbar <- sum(weights * x)/sw
+  return(sqrt(sum(weights * ((x - xbar)^2))/(sw - 1)))
 }
